727a02e3270b7ebecf74b2f0ab622f055e0aba9f
XY <15396029613@163.com>
2017-09-04 15:03:03 +0800

    FIX：删除无用的import

--- web-demo/src/main/java/com/ytfz/controller/DemoController.java
+++ web-demo/src/main/java/com/ytfz/controller/DemoController.java
@@ -1,16 +1,10 @@
 package com.ytfz.controller;
 
 import com.ytfz.service.IUserService;
-import com.ytfz.service.impl.UserServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by XY on 2017/8/30.
