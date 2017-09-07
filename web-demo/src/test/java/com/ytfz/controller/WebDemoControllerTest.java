e5abc92732eadfb567614491b40b8c0a040e21d4
XY <15396029613@163.com>
2017-09-07 10:38:06 +0800

    FIX：修改配置

--- web-demo/src/test/java/com/ytfz/controller/WebDemoControllerTest.java
+++ web-demo/src/test/java/com/ytfz/controller/WebDemoControllerTest.java
@@ -3,6 +3,7 @@ package com.ytfz.controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class) // SpringRunner包含了SpringJUnit4ClassRunner
 @SpringBootTest
-@WebMvcTest
+@AutoConfigureMockMvc
 public class WebDemoControllerTest {
     @Autowired
     private MockMvc mvc;
